0bca1a85d7d4116f56350dd96071a1ea05122e36
james598 <james@exacare.com>
2022-12-21 12:56:19 -0500

    fixes

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const bodyParser = require("body-parser")
+const cors = require('cors')
 const { faker } = require('@faker-js/faker')
 const app = express()
 const port = 3000
 
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
